f8dc669b5fa6e3c92a4c58c9d20f036ea8b6f73c
Mattew Chernov <chertchill@yandex.ru>
2025-08-02 15:37:55 +0300

    minor changes

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './app-header.module.css';
+
+class AppHeader extends React.Component {
+    render() {
+        return (
+            <header className={styles.header}>
+            
+            </header>
+        )
+    }
+}
+
+export default AppHeader;
